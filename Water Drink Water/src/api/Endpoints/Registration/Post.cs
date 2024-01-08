6a7d4837ffbdd3403ab344651d39e18ea27131d5
Terry Burns-Dyson <terrybd@gmail.com>
2024-01-08 09:01:12 -0500

    Initial projects for api/application/data.
    Added FastEndpoints and stubbed out registration end-point

---
+++ Water Drink Water/src/api/Endpoints/Registration/Post.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+
+namespace TbdFriends.WaterDrinkWater.Api.Endpoints.Registration;
+
+public class Post : Endpoint<Post.Parameters>
+{
+    public Post()
+    {
+        Post("api/registration");
+    }
+
+    public override Task HandleAsync(Parameters req, CancellationToken ct)
+    {
+        return Task.CompletedTask;
+    }
+
+    public class Parameters
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
