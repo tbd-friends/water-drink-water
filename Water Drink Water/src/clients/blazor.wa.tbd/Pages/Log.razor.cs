949fe90644b7260afa03e006e5754f776f698e20
Terry Burns-Dyson <terrybd@gmail.com>
2024-01-30 08:47:44 -0500

    Moved code from razor files to code-behind files

---
+++ Water Drink Water/src/clients/blazor.wa.tbd/Pages/Log.razor.cs
@@ -0,0 +1,15 @@
+﻿using blazor.wa.tbd.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace blazor.wa.tbd.Pages;
+
+public partial class Log
+{
+    [Inject] private UserService UserService { get; set; } = null!;
+    private int FluidOuncesConsumed { get; set; } = 0;
+
+    private async Task LogConsumption()
+    {
+        await UserService.LogConsumption(FluidOuncesConsumed);
+    }
+}
---
+++ Water Drink Water/src/clients/blazor.wa.tbd/Pages/Preferences.razor.cs
@@ -0,0 +1,15 @@
+﻿using blazor.wa.tbd.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace blazor.wa.tbd.Pages;
+
+public partial class Preferences
+{
+    [Inject] private UserService UserService { get; set; } = null!;
+    private int TargetFluidOunces { get; set; } = 0;
+
+    private async Task SavePreferences()
+    {
+        await UserService.SavePreferences(TargetFluidOunces);
+    }
+}
