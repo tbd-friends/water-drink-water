949a93dd1516b2f8f6f743b7589baa03d6c2563d
Terry Burns-Dyson <terrybd@gmail.com>
2024-02-12 08:10:46 -0500

    Add endpoint for getting progress of consumption

---
+++ Water Drink Water/src/api/Endpoints/Consumption/Get.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using TbdFriends.WaterDrinkWater.Application.Services;
+
+namespace TbdFriends.WaterDrinkWater.Api.Endpoints.Consumption;
+
+public class Get(ConsumptionService service) : EndpointWithoutRequest<Results<Ok<int>, BadRequest>>
+{
+    public override void Configure()
+    {
+        Get("api/consumption");
+    }
+
+    public override Task<Results<Ok<int>, BadRequest>> ExecuteAsync(CancellationToken ct)
+    {
+        if (int.TryParse(
+                User.Claims.SingleOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value.Split("|")[1],
+                out int userId))
+        {
+            var progress = service.GetProgressPercentage(userId);
+
+            return Task.FromResult<Results<Ok<int>, BadRequest>>(TypedResults.Ok(progress));
+        }
+
+        return Task.FromResult<Results<Ok<int>, BadRequest>>(TypedResults.BadRequest());
+    }
+}
