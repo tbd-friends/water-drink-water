5bf03e1d944ead857cfca18f0688c8d451512e1c
Nelson Segarra <junicus@outlook.com>
2024-03-04 12:50:51 -0400

    Home doesn't have the login form, it login is now it's own component

---
+++ Water Drink Water/src/clients/blazor.wa.tbd/Components/Login.razor.cs
@@ -0,0 +1,36 @@
+﻿using blazor.wa.tbd.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace blazor.wa.tbd.Components;
+
+public partial class Login
+{
+    private readonly LoginFormData formData = new();
+
+    [Inject] public UserService UserService { get; set; }
+    [Inject] public NavigationManager NavigationManager { get; set; }
+    public bool ShowAuthError { get; set; }
+    public string Error { get; set; }
+
+    public async Task ExecuteLogin()
+    {
+        ShowAuthError = false;
+
+        var result = await UserService.Authenticate(formData.Email, formData.Password);
+        if (!result)
+        {
+            Error = "Invalid email or password";
+            ShowAuthError = true;
+        }
+        else
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+
+    public class LoginFormData
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
