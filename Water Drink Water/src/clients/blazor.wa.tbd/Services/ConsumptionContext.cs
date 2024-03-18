7b1d174dc65a209c75729b2b26e65ff4ddd4d616
Nelson Segarra <junicus@outlook.com>
2024-03-18 11:55:46 -0400

    components for state provider

---
+++ Water Drink Water/src/clients/blazor.wa.tbd/Services/ConsumptionContext.cs
@@ -0,0 +1,15 @@
+﻿namespace blazor.wa.tbd.Services;
+
+public class ConsumptionContext
+{
+    public int Percentage { get; set; } = 0;
+    public event Action? OnStateChange;
+
+    public void SetPercentage(int percentage)
+    {
+        Percentage = percentage;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnStateChange?.Invoke();
+}
