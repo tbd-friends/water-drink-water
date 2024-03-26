f04f9adfec84b685649b5b42f0b325a2c8edb680
Terry Burns-Dyson <terrybd@gmail.com>
2024-03-26 08:56:17 -0400

    Add implementation of GroupRepository

---
+++ Water Drink Water/src/data/Repositories/GroupRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TbdFriends.WaterDrinkWater.Data.Contexts;
+using TbdFriends.WaterDrinkWater.Data.Contracts;
+using TbdFriends.WaterDrinkWater.Data.Models;
+
+namespace TbdFriends.WaterDrinkWater.Data.Repositories;
+
+public class GroupRepository(IDbContextFactory<ApplicationDbContext> factory) : IGroupRepository
+{
+    public void Add(Group group)
+    {
+        using var context = factory.CreateDbContext();
+
+        context.Groups.Add(group);
+
+        context.SaveChanges();
+    }
+
+    public Group? GetByName(string name, int userId)
+    {
+        using var context = factory.CreateDbContext();
+
+        return context.Groups.FirstOrDefault(g => g.Name == name && g.OwnerId == userId);
+    }
+}
