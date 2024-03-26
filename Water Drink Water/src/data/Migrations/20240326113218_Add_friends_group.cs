340a60bc6d342c7c7ae70f9d32da6ed2de2e6e6f
Terry Burns-Dyson <terrybd@gmail.com>
2024-03-26 07:33:07 -0400

    Add Group model + migration

---
+++ Water Drink Water/src/data/Migrations/20240326113218_Add_friends_group.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TbdFriends.WaterDrinkWater.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_friends_group : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Groups_Accounts_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_OwnerId",
+                table: "Groups",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
---
+++ Water Drink Water/src/data/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿namespace TbdFriends.WaterDrinkWater.Data.Models;
+
+public class Group
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Code { get; set; } = null!;
+    public int OwnerId { get; set; }
+    public DateTime DateAdded { get; set; }
+
+    public virtual Account Owner { get; set; } = null!;
+}
