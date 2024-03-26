9d23645598ff90d45094983b26857e05d3c06d25
Terry Burns-Dyson <terrybd@gmail.com>
2024-03-26 08:56:00 -0400

    Add Extension method for GetUserId on ClaimsPrincipal

---
+++ Water Drink Water/src/api/Infrastructure/ClaimExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace TbdFriends.WaterDrinkWater.Api.Infrastructure;
+
+public static class ClaimExtensions
+{
+    public static int GetUserId(this ClaimsPrincipal claimsPrincipal)
+    {
+        return int.Parse(
+            claimsPrincipal.Claims.SingleOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value.Split("|")[1] ??
+            string.Empty
+        );
+    }
+}
