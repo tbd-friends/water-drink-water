eda621eba1309c1879e4d1dc416497d17b3c23dc
Terry Burns-Dyson <terrybd@gmail.com>
2024-03-26 08:37:03 -0400

    GroupService / Tests
    Added GroupService and put tests around to validate that adding a group for a user works correctly

---
+++ Water Drink Water/src/application/Services/GroupService.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.Result;
+using TbdFriends.WaterDrinkWater.Application.Contracts;
+using TbdFriends.WaterDrinkWater.Data.Contracts;
+using TbdFriends.WaterDrinkWater.Data.Models;
+
+namespace TbdFriends.WaterDrinkWater.Application.Services;
+
+public class GroupService(IGroupRepository repository, ICodeGenerator codeGenerator)
+{
+    public Result AddNewGroup(string name, int userId)
+    {
+        var group = repository.GetByName(name, userId);
+
+        if (group is not null)
+        {
+            return Result.Conflict();
+        }
+
+        repository.Add(new Group
+        {
+            Name = name,
+            OwnerId = userId,
+            Code = codeGenerator.GenerateCode(),
+            DateAdded = DateTime.UtcNow
+        });
+
+        return Result.Success();
+    }
+}
